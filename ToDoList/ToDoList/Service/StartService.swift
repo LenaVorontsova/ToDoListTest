efaa17940321b3b7cb1a3e3d21d85423dba4e3f7
elenavorontsova <lena.voroncova26@yandex.ru>
2025-01-19 10:43:25 +0400

    delete the SceneDelegate

---
+++ ToDoList/ToDoList/Service/StartService.swift
@@ -0,0 +1,24 @@
+//
+//  StartService.swift
+//  ToDoList
+//
+//  Created by Елена Воронцова on 19.01.2025.
+//
+
+import UIKit
+
+final class StartService {
+    var window: UIWindow?
+    
+    init(window: UIWindow) {
+        self.window = window
+        configureWindow()
+    }
+    
+    func configureWindow() {
+        if let win = window {
+            win.rootViewController = ViewController()
+            win.makeKeyAndVisible()
+        }
+    }
+}
